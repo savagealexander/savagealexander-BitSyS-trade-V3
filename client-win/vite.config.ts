f0519e7d200fe9a78a66684f801b75021b86ef9c
savagealexander <32032846+savagealexander@users.noreply.github.com>
2025-08-23 20:18:27 +0800

    feat: add electron vite client

---
+++ client-win/vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  root: path.resolve(__dirname, 'src'),
+  build: {
+    outDir: path.resolve(__dirname, 'dist/renderer'),
+    emptyOutDir: true
+  }
+});
